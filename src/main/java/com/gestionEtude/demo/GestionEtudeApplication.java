c187faaf633397d604ca4588dcc724fa5aeef255
chillou-mohamed <chillou.mohamed@ump.ac.ma>
2024-01-28 19:38:31 +0100

    Configuration de la base de donnee

---
+++ src/main/java/com/gestionEtude/demo/GestionEtudeApplication.java
@@ -0,0 +1,13 @@
+package com.gestionEtude.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEtudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEtudeApplication.class, args);
+	}
+
+}
